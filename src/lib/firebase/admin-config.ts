8a3b0687446156926c430f4add65b381eea2ff93
Patrick <patzmatias.webdev@gmail.com>
2023-08-18 23:05:34 +0800

    Moved firebase admin credentials to env variables instead of service-account.json

--- src/lib/firebase/admin-config.ts
+++ src/lib/firebase/admin-config.ts
@@ -3,7 +3,14 @@ import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
 import { env } from "~/env.mjs";
 
-const firebaseAdminConfig = {credential: cert(env.GOOGLE_APPLICATION_CREDENTIALS)}
+// const firebaseAdminConfig = {credential: cert(env.GOOGLE_APPLICATION_CREDENTIALS)}
+const firebaseAdminConfig = {
+  credential: cert({
+    projectId: env.FIREBASE_PROJECT_ID,
+    clientEmail: env.FIREBASE_CLIENT_EMAIL,
+    privateKey: env.FIREBASE_PRIVATE_KEY,
+  })
+}
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseAdminConfig);
 const db = getFirestore();
