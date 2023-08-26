245b93b3595803a22a94c522fcc4fd6c22dbbdcb
Patrick <patzmatias.webdev@gmail.com>
2023-08-26 19:13:57 +0800

    Expanded viewport meta tag for proper responsive scaling

--- src/components/layout/BaseLayout/index.tsx
+++ src/components/layout/BaseLayout/index.tsx
@@ -2,14 +2,23 @@
 import type { ReactElement } from "react";
 import type { ChildProps } from "~/definitions/react";
 import SidebarProvider from "~/context/sidebar-context";
+import Head from "next/head";
 
 interface IProps extends ChildProps {}
 
 export function BaseLayout({ children }: IProps): ReactElement {
   return (
-    <SidebarProvider>
-      {children}
-    </SidebarProvider>
+    <>
+      <Head>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+        />
+      </Head>
+      <SidebarProvider>
+        {children}
+      </SidebarProvider>
+    </>
   )
 }
 
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -8,6 +8,7 @@ import {
   DASHBOARD_MAINROUTE_DETAILS, 
 } from "~/definitions/routes";
 import { useRouter } from "next/router";
+import CustomHead from "~/components/ui/CustomHead";
 
 export default function Home() {
 
@@ -16,39 +17,42 @@ export default function Home() {
   const goToRoute = (href: string) => router.push(href);
 
   return (
-    <section className="container grid px-6 mx-auto">
-      <div className="mb-8">
-        <h1>Welcome to the dashboard</h1>
-      </div>
+    <>
+      <CustomHead title="Dashboard" />
+      <section className="container grid px-6 mx-auto">
+        <div className="mb-8">
+          <h1>Welcome to the dashboard</h1>
+        </div>
 
-      <div className="cards grid grid-cols-1 md:grid-cols-1 gap-4">
-        {
-            Object.entries(DASHBOARD_MAINROUTE_DETAILS).map(([key, route]: [string, ROUTE_DETAIL], index: number)=> {
+        <div className="cards grid grid-cols-1 md:grid-cols-1 gap-4">
+          {
+              Object.entries(DASHBOARD_MAINROUTE_DETAILS).map(([key, route]: [string, ROUTE_DETAIL], index: number)=> {
 
-              const isOdd = index % 2 === 0;
-              const cardClass = isOdd ? `bg-primary text-primary-foreground` : ``;
-              const descriptionClass = isOdd ? `text-primary-foreground` : ``;
-              const buttonVariant = isOdd ? `secondary` : `default`;
-          
-              return (
-                <Card className={cardClass} key={`${key}_${index}`}>
-                  <CardHeader>
-                    <CardTitle>{route.title}</CardTitle>
-                  </CardHeader>
-                  <CardContent>
-                    <CardDescription className={descriptionClass}>
-                      {route.description}
-                    </CardDescription>
-                  </CardContent>
-                  <CardFooter className="justify-end">
-                    <Button className="flex items-center" variant={buttonVariant} onClick={() => goToRoute(route.path)}><span className="mr-1">Go</span><ArrowRight size={16} /></Button>
-                  </CardFooter>
-                </Card>
-              )
-            })
-        }
-      </div>
-    </section>
+                const isOdd = index % 2 === 0;
+                const cardClass = isOdd ? `bg-primary text-primary-foreground` : ``;
+                const descriptionClass = isOdd ? `text-primary-foreground` : ``;
+                const buttonVariant = isOdd ? `secondary` : `default`;
+            
+                return (
+                  <Card className={cardClass} key={`${key}_${index}`}>
+                    <CardHeader>
+                      <CardTitle>{route.title}</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <CardDescription className={descriptionClass}>
+                        {route.description}
+                      </CardDescription>
+                    </CardContent>
+                    <CardFooter className="justify-end">
+                      <Button className="flex items-center" variant={buttonVariant} onClick={() => goToRoute(route.path)}><span className="mr-1">Go</span><ArrowRight size={16} /></Button>
+                    </CardFooter>
+                  </Card>
+                )
+              })
+          }
+        </div>
+      </section>
+    </>
   );
 }
 
